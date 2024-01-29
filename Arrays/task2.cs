5737cdb9f0a4084ef6299f4ea6cdd5e00cde2974
Ptest <Ptest@lit.msu.ru>
2024-01-29 13:48:49 +0300

    OOPgit status!

---
+++ Arrays/task2.cs
@@ -0,0 +1,24 @@
+// Random rand = new Random();
+// int N = int.Parse(Console.ReadLine());
+// int[] Array = new int[N];
+// for (int i = 0; i<Array.Length; i++)
+// {
+//     Array[i] = rand.Next(1, 255);
+// }
+// int[] newArray = new int[Array.Length];
+// for (int i = 0; i<Array.Length; i++)
+// {
+//      Console.Write(Array[i] + " ");
+// }
+// Console.WriteLine();
+// for (int i = 0; i<Array.Length; i++)
+// {
+//     if (Math.Abs(Array[i])<100)
+//     {
+//         newArray[i] = Array[i];
+//     } 
+// }
+// for (int i = 0; i<newArray.Length; i++)
+// {
+//      Console.Write(newArray[i] + " ");
+// }
---
+++ Arrays/task3.cs
@@ -0,0 +1,45 @@
+﻿Random rand = new Random();
+//int N = int.Parse(Console.ReadLine());
+int N = 10;
+int[] Array = new int[N];
+
+for (int i = 0; i<Array.Length; i++)
+{
+    Array[i] = rand.Next(1, 6);
+}
+
+int[] newArray = new int[Array.Length];
+for (int i = 0; i<Array.Length; i++)
+{
+     Console.Write(Array[i] + " ");
+}
+Console.WriteLine();
+
+int counter = 0;
+for (int i = 0; i<Array.Length; i++)
+{
+   if (!Exists(Array[i], newArray))
+   {
+     newArray[i] = Array[counter];
+     counter += 1;
+   }
+}
+
+
+for (int i = 0; i<newArray.Length; i++)
+{
+     Console.Write(newArray[i] + " ");
+}
+
+
+bool Exists(int value, int[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        if(array[i] == value)
+        {
+           return true;
+        }
+    }
+    return false;
+}
